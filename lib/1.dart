ec98f58a8aa3c6ba44a3b7b49820d5ad25dca5ae
I S Sarath <sarathithikkat@gmail.com>
2020-10-10 06:03:43 +0530

    1

--- lib/1.dart
+++ lib/1.dart
@@ -1,12 +1,12 @@
-import 'dart:io';
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:photo_gallery/photo_gallery.dart';
 import 'package:transparent_image/transparent_image.dart';
-
-import 'albumpage.dart';
+import 'package:video_player/video_player.dart';
 
 void main() {
   runApp(MyApp());
@@ -136,5 +136,172 @@ class _MyAppState extends State<MyApp> {
               ),
       ),
     );
+  }
+}
+
+class AlbumPage extends StatefulWidget {
+  final Album album;
+
+  AlbumPage(Album album) : album = album;
+
+  @override
+  State<StatefulWidget> createState() => AlbumPageState();
+}
+
+class AlbumPageState extends State<AlbumPage> {
+  List<Medium> _media;
+
+  @override
+  void initState() {
+    super.initState();
+    initAsync();
+  }
+
+  void initAsync() async {
+    MediaPage mediaPage = await widget.album.listMedia();
+    setState(() {
+      _media = mediaPage.items;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back_ios),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          title: Text(widget.album.name),
+        ),
+        body: GridView.count(
+          crossAxisCount: 3,
+          mainAxisSpacing: 1.0,
+          crossAxisSpacing: 1.0,
+          children: <Widget>[
+            ...?_media?.map(
+              (medium) => GestureDetector(
+                onTap: () => Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => ViewerPage(medium))),
+                child: Container(
+                  color: Colors.grey[300],
+                  child: FadeInImage(
+                    fit: BoxFit.cover,
+                    placeholder: MemoryImage(kTransparentImage),
+                    image: ThumbnailProvider(
+                      mediumId: medium.id,
+                      mediumType: medium.mediumType,
+                      highQuality: true,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ViewerPage extends StatelessWidget {
+  final Medium medium;
+
+  ViewerPage(Medium medium) : medium = medium;
+
+  @override
+  Widget build(BuildContext context) {
+    DateTime date = medium.creationDate ?? medium.modifiedDate;
+    return MaterialApp(
+      
+      theme: ThemeData.dark(),
+      home: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () => Navigator.of(context).pop(),
+            icon: Icon(Icons.arrow_back_ios),
+          ),
+          title: Text(date?.toLocal().toString()),
+        ),
+        body: Container(
+          alignment: Alignment.center,
+          child: medium.mediumType == MediumType.image
+              ? FadeInImage(
+                  fit: BoxFit.cover,
+                  placeholder: MemoryImage(kTransparentImage),
+                  image: PhotoProvider(mediumId: medium.id),
+                )
+              : VideoProvider(
+                  mediumId: medium.id,
+                ),
+        ),
+      ),
+    );
+  }
+}
+
+class VideoProvider extends StatefulWidget {
+  final String mediumId;
+
+  const VideoProvider({
+    @required this.mediumId,
+  });
+
+  @override
+  _VideoProviderState createState() => _VideoProviderState();
+}
+
+class _VideoProviderState extends State<VideoProvider> {
+  VideoPlayerController _controller;
+  File _file;
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      initAsync();
+    });
+    super.initState();
+  }
+
+  Future<void> initAsync() async {
+    try {
+      _file = await PhotoGallery.getFile(mediumId: widget.mediumId);
+      _controller = VideoPlayerController.file(_file);
+      _controller.initialize().then((_) {
+        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+        setState(() {});
+      });
+    } catch (e) {
+      print("Failed : $e");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _controller == null || !_controller.value.initialized
+        ? Container()
+        : Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              AspectRatio(
+                aspectRatio: _controller.value.aspectRatio,
+                child: VideoPlayer(_controller),
+              ),
+              FlatButton(
+                onPressed: () {
+                  setState(() {
+                    _controller.value.isPlaying
+                        ? _controller.pause()
+                        : _controller.play();
+                  });
+                },
+                child: Icon(
+                  _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+                ),
+              ),
+            ],
+          );
   }
 }
---
+++ lib/viewer.dart
@@ -0,0 +1,115 @@
+
+import 'dart:io';
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:photo_gallery/photo_gallery.dart';
+import 'package:transparent_image/transparent_image.dart';
+import 'package:video_player/video_player.dart';
+import 'package:flutter_share/flutter_share.dart';
+
+class ViewerPage extends StatelessWidget {
+  final Medium medium;
+
+  ViewerPage(Medium medium) : medium = medium;
+
+  @override
+  Widget build(BuildContext context) {
+    DateTime date = medium.creationDate ?? medium.modifiedDate;
+    return MaterialApp(
+      
+      theme: ThemeData.dark(),
+      home: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () => Navigator.of(context).pop(),
+            icon: Icon(Icons.arrow_back_ios),
+          ),
+          title: Text(date?.toLocal().toString()),
+          actions: [
+            IconButton(
+              icon:Icon(Icons.share),
+              onPressed: shareFile,
+              )
+          ],
+        ),
+        body: Container(
+          alignment: Alignment.center,
+          child: medium.mediumType == MediumType.image
+              ? FadeInImage(
+                  fit: BoxFit.cover,
+                  placeholder: MemoryImage(kTransparentImage),
+                  image: PhotoProvider(mediumId: medium.id),
+                )
+              : VideoProvider(
+                  mediumId: medium.id,
+                ),
+        ),
+      ),
+    );
+  }
+}
+
+class VideoProvider extends StatefulWidget {
+  final String mediumId;
+
+  const VideoProvider({
+    @required this.mediumId,
+  });
+
+  @override
+  _VideoProviderState createState() => _VideoProviderState();
+}
+
+class _VideoProviderState extends State<VideoProvider> {
+  VideoPlayerController _controller;
+  File _file;
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      initAsync();
+    });
+    super.initState();
+  }
+
+  Future<void> initAsync() async {
+    try {
+      _file = await PhotoGallery.getFile(mediumId: widget.mediumId);
+      _controller = VideoPlayerController.file(_file);
+      _controller.initialize().then((_) {
+        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+        setState(() {});
+      });
+    } catch (e) {
+      print("Failed : $e");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _controller == null || !_controller.value.initialized
+        ? Container()
+        : Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              AspectRatio(
+                aspectRatio: _controller.value.aspectRatio,
+                child: VideoPlayer(_controller),
+              ),
+              FlatButton(
+                onPressed: () {
+                  setState(() {
+                    _controller.value.isPlaying
+                        ? _controller.pause()
+                        : _controller.play();
+                  });
+                },
+                child: Icon(
+                  _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+                ),
+              ),
+            ],
+          );
+  }
+}
